94f31c666d410a0521951fb1156e4f211ed298ce
terrence <chengtinghua@coding.com>
2017-04-03 12:51:08 +0800

    [init]项目初始化

--- publish.sh
+++ publish.sh
@@ -12,4 +12,4 @@ if [[ `docker images -a | grep $DOCKER_REGISTRY_TEST/$DOCKER_REPOSITORY` ]];then
 fi
 mvn clean install -Ptest -Dmaven.test.skip=true -Ddocker.registry=$DOCKER_REGISTRY_TEST -Ddocker.repository=$DOCKER_REPOSITORY -Ddocker.tag=$DOCKER_TAG
 #docker run -d -p 8080:80 -v /var/log/case_webapp:/var/log/case_webapp --restart=always $DOCKER_REGISTRY_TEST/$DOCKER_REPOSITORY:$DOCKER_TAG
-docker run -d -p 8080:8080 --restart=always $DOCKER_REGISTRY_TEST/$DOCKER_REPOSITORY:$DOCKER_TAG
+docker run -d -p 8081:8080 --restart=always $DOCKER_REGISTRY_TEST/$DOCKER_REPOSITORY:$DOCKER_TAG
